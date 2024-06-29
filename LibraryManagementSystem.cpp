807bfd39ed18fccabf78977a59e10ccbde6eba4f
Tanishq Patidar <tanishq.patidar@kalvium.community>
2024-06-29 15:45:44 +0530

    feat: Add abstract class for LibraryItem
    This commit adds an abstract class called LibraryItem to the LibraryManagementSystem.cpp file. The LibraryItem class provides a common interface for different types of library items and includes member variables for title and id. It also includes a pure virtual function called display() which will be implemented by derived classes. This abstract class will serve as the base for future library item implementations.

--- LibraryManagementSystem.cpp
+++ LibraryManagementSystem.cpp
@@ -3,6 +3,18 @@
 #include <vector>
 #include <algorithm>
 
+// Abstract Class
+class LibraryItem {
+protected:
+    std::string title;
+    std::string id;
+
+public:
+    LibraryItem(const std::string& title, const std::string& id) : title(title), id(id) {}
+    virtual void display() const = 0; // Pure virtual function
+    virtual ~LibraryItem() {}
+};
+
 // Main Function
 int main() {
 
